5fda27e307d344a3e65b496944cf859b27156c69
quimpm <quimpm99@gmail.com>
2021-11-06 13:52:44 +0100

    createdb script

--- commed/offer/management/commands/createdb.py
+++ commed/offer/management/commands/createdb.py
@@ -2,7 +2,8 @@ import os
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from offer.models import Encounter, FormalOffer
-from product.models import Product
+from product.models import Product, Category, Tag, ProductImage
+from enterprise.models import Enterprise
 from unittest.mock import MagicMock
 from django.core.files import File
 
@@ -29,16 +30,58 @@ class Command(BaseCommand):
 
         product1 = Product.objects.create(
             owner=user_2,
-            description="gertgtg3",
+            description="Bananas",
             latitude=0.0,
             longitude=0.0
         )
         product2 = Product.objects.create(
             owner=user_1,
-            description="ekjpoeasjdmpa",
+            description="Pineaples",
             latitude=0.0,
             longitude=0.0
         )
+        apple_tag = Tag.objects.create(
+            name="apple"
+        )
+        apples_tag = Tag.objects.create(
+            name="apples"
+        )
+        pear_tag = Tag.objects.create(
+            name="pear"
+        )
+        pears_tag = Tag.objects.create(
+            name="pears"
+        )
+        dishwasher_tag = Tag.objects.create(
+            name="dishwasher"
+        )
+        microwave_tag = Tag.objects.create(
+            name="microwave"
+        )
+        cat_apple = Category.objects.create(
+            name="apple",
+        )
+        cat_apple.tag_children.set([apple_tag, apples_tag, pear_tag, pears_tag])
+        cat_dishwasher = Category.objects.create(
+            name="dishwasher",
+        )
+        cat_dishwasher.tag_children.set([dishwasher_tag, microwave_tag])
+        self.fruit = Product.objects.create(
+            owner=user_2,
+            title="Some fruit",
+            description="gertgtg3",
+            latitude=0.0,
+            longitude=0.0,
+        )
+        self.fruit.tag.set([apple_tag, apples_tag, pear_tag, pears_tag])
+        self.microwave = Product.objects.create(
+            owner=user_1,
+            title="Good Microwave",
+            description="ekjpoeasjdmpa",
+            latitude=2.0,
+            longitude=3.0
+        )
+        self.microwave.tag.set([dishwasher_tag, microwave_tag])
         client1 = User.objects.create(
             username="emina",
             password="complexpass",
@@ -61,7 +104,6 @@ class Command(BaseCommand):
             client=client2,
             product=product2
         )
-        
         # FormalOffer
         file_mock = MagicMock(spec=File)
         file_mock.name = 'test.pdf'
@@ -77,4 +119,17 @@ class Command(BaseCommand):
             contract = "adsfasdf",
             signedPdf = file_mock
         )
-
+        Enterprise.objects.create(
+            owner=user_1,
+            NIF="12345678X",
+            name="Restaurant Paco",
+            contactInfo="paco@paco.com",
+            description="<strong>This is a strong statement, lady</strong>",
+        )
+        Enterprise.objects.create(
+            owner=user_2,
+            NIF="21345678X",
+            name="Restaurant PaNco",
+            contactInfo="paco@paco.com",
+            description="<strong>This is a strong statement, lady</strong>",
+        )
