3c2e0136cec04cf9dcef95a3caab412bb13c88c8
quimpm <quimpm99@gmail.com>
2021-11-14 12:36:24 +0100

    some fixes to be done in the backend

--- commed/commed/settings.py
+++ commed/commed/settings.py
@@ -73,7 +73,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
