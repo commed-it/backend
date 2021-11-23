b40f63e597b47575d8f4e28220c40c92b2033457
quimpm <quimpm99@gmail.com>
2021-11-23 23:04:01 +0100

    Created recomendation endpoint

--- commed/product/urls.py
+++ commed/product/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import ProductViewSet, SearchView, UserProducts
+from .views import ProductViewSet, SearchView, UserProducts, RecomendationView
 
 router = SimpleRouter()
 router.register("", ProductViewSet)
 
 urlpatterns = [ path('search/', SearchView.as_view()),
+                path('recomendation/', RecomendationView.as_view()),
                 path('user/<int:user_id>', UserProducts.as_view()),
                 ] + router.urls
