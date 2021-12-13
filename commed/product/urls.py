0fcf200d4638e00cf0e85856c1d0d4747adac9fd
quimpm <quimpm99@gmail.com>
2021-12-13 18:51:09 +0100

    fixed stuff

--- commed/product/urls.py
+++ commed/product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import ProductViewSet, SearchView, UserProducts, RecomendationView
+from .views import ProductViewSet, SearchView, UserProducts, RecomendationView, ProductImageViewSet
 
 router = SimpleRouter()
 router.register("", ProductViewSet)
+router.register("images", ProductImageViewSet)
 
 urlpatterns = [ path('search/', SearchView.as_view()),
                 path('recomendation/', RecomendationView.as_view()),
