cfb0031b41bc36c455514e141364cffb1c0b5cd8
Sergi Simon Balcells <sergisiba@gmail.com>
2021-12-14 14:16:11 +0100

    This two lines of code took 2 hours.

--- commed/offer/urls.py
+++ commed/offer/urls.py
@@ -7,11 +7,12 @@ router = SimpleRouter()
 router.register("encounter", EncounterViewSet)
 router.register("formaloffer", FormalOfferViewSet)
 
+
 urlpatterns = router.urls + \
     [
         path('formaloffer/fromUser/<int:user_id>', FormalOfferFromUserViewSet.as_view({'get': 'list'})),
         path('encounter/fromUser/<int:user_id>', ListChatsViewSet.as_view({'get': 'list'})),
-        path('encounter/create-if-not-exists/', CreateIfNotExistsEncounter.as_view({'post': 'create'})),
+        path('encounter/create-if-not-exists', CreateIfNotExistsEncounter.as_view()),
         path('encounter/user/<int:user_id>', UserEncounter.as_view()),
         path('formaloffer/user/<int:user_id>', UserFormalOffers.as_view()),
     ]
