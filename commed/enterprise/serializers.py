99bd50a199a41dc7b052b84db6c80c25a8424a57
quimpm <quimpm99@gmail.com>
2021-12-14 01:07:58 +0100

    added default to description and updated profile serializer for images

--- commed/enterprise/serializers.py
+++ commed/enterprise/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from .models import Enterprise
-
+from product.serializers import Base64ImageField
 
 class EnterpriseSerializer(serializers.ModelSerializer):
+    profileImage = Base64ImageField(read_only = False)
+    bannerImage = Base64ImageField(read_only = False)
+
     class Meta:
         model = Enterprise
         fields = ("id", "owner", "NIF", "name", "contactInfo", "description", "profileImage", "bannerImage")
