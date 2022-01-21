5ccf2f109d2ddda2ccbd1c06236bc7ba23446a9f
Oriol <oriolalascercos@gmail.com>
2022-01-21 18:46:32 +0100

    fixing some bugs

--- commed/heroku-script.sh
+++ commed/heroku-script.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-python3 commed/manage.py collectstatic --no-input
-
-python3 commed/manage.py flush --no-input
-
-python3 commed/manage.py makemigrations
-
-python3 commed/manage.py migrate
-
-python3 commed/manage.py createdb
-
-python3 commed/manage.py initadmin
-
-python3 commed/server.py
+python3 commed/manage.py collectstatic --no-input && python3 commed/manage.py flush --no-input && python3 commed/manage.py makemigrations && python3 commed/manage.py migrate && python3 commed/manage.py createdb && python3 commed/manage.py initadmin && python3 commed/server.py
