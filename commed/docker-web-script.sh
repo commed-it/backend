79a06da15b020c6c6882b874598e7f1031fa1462
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2022-01-21 16:11:19 +0100

    Update docker-web-script.sh

--- commed/docker-web-script.sh
+++ commed/docker-web-script.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-python3 manage.py collectstatic --no-input
+python3 commed/manage.py collectstatic --no-input
 
-python3 manage.py flush --no-input
+python3 commed/manage.py flush --no-input
 
-python3 manage.py makemigrations
+python3 commed/manage.py makemigrations
 
-python3 manage.py migrate
+python3 commed/manage.py migrate
 
-python3 manage.py createdb
+python3 commed/manage.py createdb
 
-python3 manage.py initadmin
+python3 commed/manage.py initadmin
 
-python3 server.py
+python3 commed/server.py
